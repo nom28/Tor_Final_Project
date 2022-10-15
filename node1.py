5878c5ddc3cd005ecffc2a75ced7ed61a3c4d823
Noam Schuldiner <noamschuldiner@gmail.com>
2022-10-15 14:23:45 +0300

    image transfer is working between 2 clients + 3 nodes!! with full 3 layer encryption. good proggress, though still without the directory server - the ports are hardcoded which does need to be changed

---
+++ node1.py
@@ -0,0 +1,46 @@
+from scapy.all import *
+from threading import Thread
+import time
+from queue import Queue, Empty
+from scapy.layers.inet import *
+
+
+from tools.layer import Layer
+
+data = b""
+node_layer = Layer()
+node_layer.change_keys("1")
+
+finished = False
+
+
+def threaded_sniff_with_send():
+    q = Queue()
+    sniffer = Thread(target=sniff_loopback, args=(q,))
+    sniffer.daemon = True
+    sniffer.start()
+    time.sleep(1)  # just to make sure the sniffer doesn't override with future scapy functions.
+    while not finished:
+        try:
+            pkt = q.get(timeout=1)
+            pkt = decrypt_packet(pkt.load)
+            send_data(pkt)
+        except (Empty, AttributeError):
+            pass
+
+
+def sniff_loopback(q):
+    sniff(prn=lambda x: q.put(x), filter="dst port 55556", iface="Software Loopback Interface 1")
+
+
+def decrypt_packet(data):
+    decrypted_data = node_layer.decrypt(data)
+    return decrypted_data
+
+
+def send_data(data):
+    packet = IP(dst="127.0.0.1") / TCP(dport=55557, sport=55556) / Raw(data)
+    send(packet)
+
+
+threaded_sniff_with_send()
