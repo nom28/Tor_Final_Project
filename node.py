d45de9d5c29385254d5f1e1cab1878beec684a3c
Noam Schuldiner <noamschuldiner@gmail.com>
2023-05-04 13:19:45 +0300

    nodes have been changed to support multiple clients

--- node.py
+++ node.py
@@ -5,6 +5,8 @@ import time
 from queue import Queue, Empty
 
 from tools.layer_new import Layer
+from tools.bidict import BiDict
+import tools.toolbox as tb
 
 warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
 from scapy.all import *
@@ -26,57 +28,54 @@ src_to_sk = {}
 
 def threaded_sniff_with_send():
     q = Queue()
+    prev_addr_to_ports = BiDict()
+
     print("initiating sniffer")
     sniffer = Thread(target=sniff_loopback, args=(q,))
     sniffer.daemon = True
     sniffer.start()
     time.sleep(1)  # just to make sure the sniffer doesn't override with future scapy functions.
     print("sniffer initiated - listening")
+
     while not finished:
         if not q.empty():
-            try:
-                pkt = q.get(timeout=1)
-                if TCP not in pkt:
-                    continue
-                if pkt[TCP].ack == 1:
-                    print("ack")
-                    continue
-                # pkt.show()
-                src = pkt[IP].src
-                sport = pkt[TCP].sport
-                pkt_address = stringify([src, sport])
-                print(pkt_address)
-                if not previous_comp_address:
-                    previous_comp_address.append(src)
-                    previous_comp_address.append(sport)
-                # if src == previous_comp_address[0] and sport == previous_comp_address[1]:
-                if pkt_address in sk_to_dst.values() or pkt_address not in src_to_sk:
-                    pkt = decrypt_packet(pkt.load)
-                    ip, port, session_id, data = pkt
+            pkt = q.get(timeout=1)
+            # Checks if is a data TCP packet
+            if TCP not in pkt:
+                continue
+            if pkt[TCP].ack == 1:
+                print("ack")
+                continue
 
-                    if session_id not in src_to_sk.values():
-                        src_to_sk[stringify([ip, port])] = session_id
-                    if session_id not in sk_to_dst:
-                        sk_to_dst[session_id] = pkt_address
+            dport = pkt[TCP].dport
+            sport = pkt[TCP].sport
+            src = pkt[IP].src
+            src_address = stringify([src, sport])
 
-                    send_data(data, ip, port)
-                    print(f"{key_num}-->")
-                else:
-                    if pkt_address not in src_to_sk:
-                        print("very interesting")
-                        exit()
-                    session_id = src_to_sk[pkt_address]
-                    address = eval(sk_to_dst[session_id])
-                    data = encrypt_packet(pkt.load)
-                    send_data(data, address[0], int(address[1]))
-                    print(f"{address}<--{key_num}")
-            except AttributeError:
-                pass
+            if dport == personal_port:
+                if not prev_addr_to_ports.has_this_key(src_address):  # If this is the first message then add it to the dict
+                    available_port = tb.find_next_available_port(start_port)
+                    print(available_port)
+                    prev_addr_to_ports.add(src_address, available_port)
+
+                pkt = decrypt_packet(pkt.load)
+                if key_num == "3":
+                    print("pkt3", pkt)
+                ip, port, session_id, data = pkt  # session key becomes redundant
+                print(f"{key_num}-->{ip}:{port}")
+                send_data(data, ip, port, prev_addr_to_ports.get_value(src_address))
+            elif prev_addr_to_ports.has_this_value(dport):
+                ip, port = eval(prev_addr_to_ports.get_key(dport))
+                print(f"{ip}:{port}<--{key_num}")
+                data = encrypt_packet(pkt.load)
+                send_data(data, ip, int(port), personal_port)
+            else:  # if packet is not meant for the platform
+                send(pkt)
 
 
 # "Software Loopback Interface 1"
 def sniff_loopback(q):
-    sniff(prn=lambda x: q.put(x), filter=f"dst port {personal_port}", iface="\\Device\\NPF_Loopback")
+    sniff(prn=lambda x: q.put(x), iface="\\Device\\NPF_Loopback")
 
 
 def decrypt_packet(data):
@@ -89,8 +88,8 @@ def encrypt_packet(data):
     return encrypted_data
 
 
-def send_data(data, ip, port):
-    packet = IP(dst=ip) / TCP(dport=port, sport=personal_port) / Raw(data)
+def send_data(data, ip, port, cport):
+    packet = IP(dst=ip) / TCP(dport=port, sport=cport) / Raw(data)
     send(packet)
 
 
@@ -103,7 +102,8 @@ def stringify(lst):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         personal_port = int(sys.argv[1])
-        key_num = sys.argv[2]
+        start_port = int(sys.argv[2])
+        key_num = sys.argv[3]
 
         node_layer.change_keys(key_num)
     threaded_sniff_with_send()
--- node_cluster.py
+++ node_cluster.py
@@ -2,17 +2,20 @@ import subprocess
 from threading import *
 import random
 port = 55556
+default = 55560
 nodes = []
+NUMBER_OF_NODES = 3
 
 
-def run_node(port, key_num):
-    subprocess.run(f"python node.py {port} {key_num}")
+def run_node(port, default, key_num):
+    subprocess.run(f"python node.py {port} {default} {key_num}")
 
 
-for x in range(1, 4):
-    nodes.append(Thread(target=run_node, args=(port, x)))
+for x in range(1, NUMBER_OF_NODES + 1):
+    nodes.append(Thread(target=run_node, args=(port, default, x)))
     # nodes[x-1].daemon = True
     nodes[x-1].start()
+    default += 10
     port += 1
 print(nodes)
 
